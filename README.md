d996addb50e148dfa3dd5f0057c4633ca1a7f495
Pavithra Sinha <to.pavithra04@gmail.com>
2020-09-08 11:40:26 +0530

    created the title of the page

--- README.md
+++
@@ -1,3 +0,0 @@
-# mybooklist
-# mybooklist
-# mybooklist
---
+++ sandbox.js
@@ -0,0 +1,8 @@
+const names = ['Tom', 'Jerry', 'Spike', 'Pokemon', 'Doremon']
+const password = 'Pass';
+
+if (password.length > 8) {
+    console.log('Password is long enough!!!')
+} else {
+    console.log('password is too small')
+}
